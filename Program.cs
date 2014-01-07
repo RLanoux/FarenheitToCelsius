71e2d51ee9d0ff0686d2450e106da553ec608db4
Raymond Lanoux <raymondlanoux@gmail.com>
2014-01-06 18:31:43 -0600

    initial add

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FarenheitToCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declare variables
+            double Fahrenheit = 87;
+            double Celsius;
+
+            //Calculate the temperature in Celsius
+            Celsius = (Fahrenheit - 32) * 5 / 9;
+
+            //Show what the temperature is
+            Console.WriteLine("{0:##.###} degrees Fahrenheit converts to " +
+                              "{1:##.###} degrees Celsius.", Fahrenheit, Celsius);
+            Console.ReadLine();
+        }
+    }
+}
